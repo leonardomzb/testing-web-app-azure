bc9f5b82d611776865e1ee38b15d36f2d875d25b
leonardomzb <leonardo.zamorano.b@gmail.com>
2026-02-26 14:18:28 -0300

    First

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace testing_web_app_azure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
